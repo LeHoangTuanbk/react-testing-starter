045a854f16a1c315d780d71a530ddd077613d8f0
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-07-14 21:12:38 +0900

    Add: refactor test

--- tests/components/OrderStatusSelect.test.tsx
+++ tests/components/OrderStatusSelect.test.tsx
@@ -2,38 +2,33 @@ import { Theme } from '@radix-ui/themes';
 import { render, screen } from '@testing-library/react';
 import OrderStatusSelector from '../../src/components/OrderStatusSelector';
 import userEvent from '@testing-library/user-event';
-import { late } from 'zod';
 
 describe('OrderStatusSelector', () => {
-  it('should render New as the default value', async () => {
+  const renderComponent = () => {
     render(
       <Theme>
         <OrderStatusSelector onChange={vi.fn()} />
       </Theme>
     );
 
-    const button = screen.getByRole('combobox');
+    return {
+      trigger: screen.getByRole('combobox'),
+      getOptions: () => screen.findAllByRole('option'),
+    };
+  };
 
-    expect(button).toHaveTextContent(/new/i);
+  it('should render New as the default value', async () => {
+    const { trigger } = renderComponent();
+    expect(trigger).toHaveTextContent(/new/i);
   });
 
   it('should render correct statuses', async () => {
-    render(
-      <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
-      </Theme>
-    );
-
-    const button = screen.getByRole('combobox');
-
-    expect(button).toHaveTextContent(/new/i);
+    const { trigger, getOptions } = renderComponent();
 
     const user = userEvent.setup();
-    await user.click(button);
+    await user.click(trigger);
 
-    const options = await screen.findAllByRole('option');
-
-    expect(options.length).toBe(3);
+    const options = await getOptions();
 
     const optionLabels = options.map((option) => option.textContent);
 
