cebf9cb8313f5e0c15aab93930c61441feac74fa
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-07-15 21:00:05 +0900

    Add: mock data

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -6,8 +6,20 @@ import {
 import ProductList from '../../src/components/ProductList';
 import { http, HttpResponse, delay } from 'msw';
 import { server } from '../mocks/server';
+import { db } from '../mocks/db';
 
 describe('ProductList', () => {
+  const productIds: number[] = [];
+  beforeAll(() => {
+    [1, 2, 3].forEach(() => {
+      const product = db.product.create();
+      productIds.push(product.id);
+    });
+  });
+  afterAll(() => {
+    db.product.deleteMany({ where: { id: { in: productIds } } });
+  });
+
   it('should render the list of products', async () => {
     render(<ProductList />);
 
